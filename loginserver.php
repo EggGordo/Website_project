237a1e47f1860dfaad530cbd3da8202df7ecda8b
Jackson Spaugh <jacksonspaugh13@gmail.com>
2023-03-14 11:11:46 -0400

    Delete loginserver.php

--- loginserver.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sname="fdb29.awardspace.net";
-$uname="4175161_emporiummaster";
-$pass="OMITTED";
-$dbname="4175161_emporiummaster";
-$conn = mysqli_connect($sname,$uname,$pass,$dbname);
-if (!$conn) {
-    echo "failure!";
-}
