edbf3dda6f2c977bb7f5e7927f51e7065adc5e1f
egggordo <jacksonspaugh13@gmail.com>
2023-03-14 11:47:40 -0400

    Change is good

--- dbConn.php
+++ dbConn.php
@@ -1,7 +1,7 @@
 <?php
 $sname="fdb29.awardspace.net";
 $uname="4175161_emporiummaster";
-$pass="xgQN9SYc7v:+x1s3";
+$pass="password"; //Place holder pass
 $dbname="4175161_emporiummaster";
 $conn = mysqli_connect($sname,$uname,$pass,$dbname);
 if(!$conn) {
--- loginserver.php
+++ loginserver.php
@@ -2,7 +2,7 @@
 <?php
 $sname="fdb29.awardspace.net";
 $uname="4175161_emporiummaster";
-$pass="OMITTED";
+$pass="password"; //reminder to change the old password
 $dbname="4175161_emporiummaster";
 $conn = mysqli_connect($sname,$uname,$pass,$dbname);
 if (!$conn) {
