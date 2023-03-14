3ce524d8a6fb30ab83885f39871305b0af7e5400
egggordo <jacksonspaugh13@gmail.com>
2023-03-14 11:12:39 -0400

    I am terrbile at secuirty

---
+++ loginserver.php
@@ -0,0 +1,10 @@
+//this file logins to the php server
+<?php
+$sname="fdb29.awardspace.net";
+$uname="4175161_emporiummaster";
+$pass="OMITTED";
+$dbname="4175161_emporiummaster";
+$conn = mysqli_connect($sname,$uname,$pass,$dbname);
+if (!$conn) {
+    echo "failure!";
+}
